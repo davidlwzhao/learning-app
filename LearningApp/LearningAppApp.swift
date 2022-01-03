0362f04bff6b14311e7c286b95395f7adf04d6df
David Zhao <david.zhao@hotmail.co.uk>
2022-01-03 13:18:34 +0000

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by David Zhao on 03/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
